c465651baf40abea992a410a5705ca29d1ee6d85
salty-max <max@jellycat.fr>
2023-02-05 22:05:21 +0100

    adds standard-verison

---
+++ .versionrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  types: [
+    { type: 'feat', section: 'New features' },
+    { type: 'fix', section: 'Bug fixes' },
+    { type: 'change', section: 'Changes' },
+    { type: 'style', section: 'Styling changes' },
+    { type: 'chore', hidden: true },
+    { type: 'docs', hidden: true },
+    { type: 'perf', hidden: true },
+    { type: 'test', hidden: true },
+  ],
+};
