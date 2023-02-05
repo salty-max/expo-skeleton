1da10499af42dd061182cd783e72508a4a5e2f46
salty-max <max@jellycat.fr>
2023-02-05 22:00:11 +0100

    adds ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    'universe',
+    'universe/shared/typescript-analysis',
+    'plugin:react-hooks/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
+  settings: {
+    'import-resolver': {
+      typescript: {}, // This loads <rootdir>/tsconfig.json to ESLint
+    },
+  },
+  /* for lint-staged */
+  globals: {
+    __dirname: true,
+  },
+};
